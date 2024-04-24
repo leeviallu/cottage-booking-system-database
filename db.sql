46ba5c79d516344bd9d87b62248c972a503365fc
Lauri Kuivisto <158526326+Laurik22@users.noreply.github.com>
2024-04-24 15:45:15 +0300

    Update db.sql
    Uusi versio tietokannasta: keijon muuuttama + palvelu_id auto increment

--- db.sql
+++ db.sql
@@ -1,9 +1,10 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema vn
 -- -----------------------------------------------------
@@ -12,8 +13,23 @@ DROP SCHEMA IF EXISTS `vn` ;
 -- -----------------------------------------------------
 -- Schema vn
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `vn` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE SCHEMA IF NOT EXISTS `vn` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin ;
 USE `vn` ;
+
+-- -----------------------------------------------------
+-- Table `vn`.`alue`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `vn`.`alue` ;
+
+CREATE TABLE IF NOT EXISTS `vn`.`alue` (
+  `alue_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nimi` VARCHAR(40) NULL DEFAULT NULL,
+  PRIMARY KEY (`alue_id`),
+  INDEX `alue_nimi_index` (`nimi` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
+
 
 -- -----------------------------------------------------
 -- Table `vn`.`posti`
@@ -22,9 +38,11 @@ DROP TABLE IF EXISTS `vn`.`posti` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`posti` (
   `postinro` CHAR(5) NOT NULL,
-  `toimipaikka` VARCHAR(45) NULL,
+  `toimipaikka` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`postinro`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
 
 
 -- -----------------------------------------------------
@@ -33,7 +51,7 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `vn`.`asiakas` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`asiakas` (
-  `asiakas_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `asiakas_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `postinro` CHAR(5) NOT NULL,
   `etunimi` VARCHAR(20) NULL DEFAULT NULL,
   `sukunimi` VARCHAR(40) NULL DEFAULT NULL,
@@ -41,29 +59,17 @@ CREATE TABLE IF NOT EXISTS `vn`.`asiakas` (
   `email` VARCHAR(50) NULL DEFAULT NULL,
   `puhelinnro` VARCHAR(15) NULL DEFAULT NULL,
   PRIMARY KEY (`asiakas_id`),
-  INDEX `fk_as_posti1_idx` (`postinro` ASC),
-  INDEX `asiakas_snimi_idx` (`sukunimi` ASC),
-  INDEX `asiakas_enimi_idx` (`etunimi` ASC),
+  INDEX `fk_as_posti1_idx` (`postinro` ASC) VISIBLE,
+  INDEX `asiakas_snimi_idx` (`sukunimi` ASC) VISIBLE,
+  INDEX `asiakas_enimi_idx` (`etunimi` ASC) VISIBLE,
   CONSTRAINT `fk_asiakas_posti`
     FOREIGN KEY (`postinro`)
     REFERENCES `vn`.`posti` (`postinro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `vn`.`alue`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `vn`.`alue` ;
-
-CREATE TABLE IF NOT EXISTS `vn`.`alue` (
-  `alue_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nimi` VARCHAR(40) NULL DEFAULT NULL,
-  PRIMARY KEY (`alue_id`),
-  INDEX `alue_nimi_index` (`nimi` ASC))
-ENGINE = InnoDB;
-
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
 
 
 -- -----------------------------------------------------
@@ -73,17 +79,17 @@ DROP TABLE IF EXISTS `vn`.`mokki` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`mokki` (
   `mokki_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `alue_id` INT(11) UNSIGNED NOT NULL,
+  `alue_id` INT UNSIGNED NOT NULL,
   `postinro` CHAR(5) NOT NULL,
-  `mokkinimi` VARCHAR(45) NULL,
-  `katuosoite` VARCHAR(45) NULL,
-	`hinta` DOUBLE(8,2) NOT NULL,
-  `kuvaus` VARCHAR(150) NULL,
-  `henkilomaara` INT NULL,
-  `varustelu` VARCHAR(100) NULL,
+  `mokkinimi` VARCHAR(45) NULL DEFAULT NULL,
+  `katuosoite` VARCHAR(45) NULL DEFAULT NULL,
+  `hinta` DOUBLE(8,2) NOT NULL,
+  `kuvaus` VARCHAR(150) NULL DEFAULT NULL,
+  `henkilomaara` INT NULL DEFAULT NULL,
+  `varustelu` VARCHAR(100) NULL DEFAULT NULL,
   PRIMARY KEY (`mokki_id`),
-  INDEX `fk_mokki_alue_idx` (`alue_id` ASC),
-  INDEX `fk_mokki_posti_idx` (`postinro` ASC),
+  INDEX `fk_mokki_alue_idx` (`alue_id` ASC) VISIBLE,
+  INDEX `fk_mokki_posti_idx` (`postinro` ASC) VISIBLE,
   CONSTRAINT `fk_mokki_alue`
     FOREIGN KEY (`alue_id`)
     REFERENCES `vn`.`alue` (`alue_id`)
@@ -94,7 +100,9 @@ CREATE TABLE IF NOT EXISTS `vn`.`mokki` (
     REFERENCES `vn`.`posti` (`postinro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
 
 
 -- -----------------------------------------------------
@@ -103,27 +111,30 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `vn`.`varaus` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`varaus` (
-  `varaus_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `asiakas_id` INT(11) UNSIGNED NOT NULL,
-  `mokki_mokki_id` INT UNSIGNED NOT NULL,
+  `varaus_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `asiakas_id` INT UNSIGNED NOT NULL,
+  `mokki_id` INT UNSIGNED NOT NULL,
   `varattu_pvm` DATETIME NULL DEFAULT NULL,
   `vahvistus_pvm` DATETIME NULL DEFAULT NULL,
   `varattu_alkupvm` DATETIME NULL DEFAULT NULL,
   `varattu_loppupvm` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`varaus_id`),
-  INDEX `varaus_as_id_index` (`asiakas_id` ASC),
-  INDEX `fk_var_mok_idx` (`mokki_mokki_id` ASC),
+  INDEX `varaus_as_id_index` (`asiakas_id` ASC) VISIBLE,
+  INDEX `fk_var_mok_idx` (`mokki_id` ASC) VISIBLE,
+  CONSTRAINT `fk_varaus_mokki`
+    FOREIGN KEY (`mokki_id`)
+    REFERENCES `vn`.`mokki` (`mokki_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `varaus_ibfk`
     FOREIGN KEY (`asiakas_id`)
     REFERENCES `vn`.`asiakas` (`asiakas_id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_varaus_mokki`
-    FOREIGN KEY (`mokki_mokki_id`)
-    REFERENCES `vn`.`mokki` (`mokki_id`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
+
 
 -- -----------------------------------------------------
 -- Table `vn`.`lasku`
@@ -131,17 +142,19 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `vn`.`lasku` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`lasku` (
-  `lasku_id` INT(11) NOT NULL,
-  `varaus_id` INT(11) UNSIGNED NOT NULL,
+  `lasku_id` INT NOT NULL,
+  `varaus_id` INT UNSIGNED NOT NULL,
   `summa` DOUBLE(8,2) NOT NULL,
   `alv` DOUBLE(8,2) NOT NULL,
-  `maksettu` TINYINT(1) NOT NULL DEFAULT 0,
+  `maksettu` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`lasku_id`),
+  INDEX `lasku_ibfk_1` (`varaus_id` ASC) VISIBLE,
   CONSTRAINT `lasku_ibfk_1`
     FOREIGN KEY (`varaus_id`)
-    REFERENCES `vn`.`varaus` (`varaus_id`)
-    ON DELETE RESTRICT)
-ENGINE = InnoDB;
+    REFERENCES `vn`.`varaus` (`varaus_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
 
 
 -- -----------------------------------------------------
@@ -150,22 +163,23 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `vn`.`palvelu` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`palvelu` (
-  `palvelu_id` INT(11) UNSIGNED NOT NULL,
-  `alue_id` INT(11) UNSIGNED NOT NULL,
+  `palvelu_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `alue_id` INT UNSIGNED NOT NULL,
   `nimi` VARCHAR(40) NULL DEFAULT NULL,
-  `tyyppi` INT(11) NULL DEFAULT NULL,
   `kuvaus` VARCHAR(255) NULL DEFAULT NULL,
   `hinta` DOUBLE(8,2) NOT NULL,
   `alv` DOUBLE(8,2) NOT NULL,
   PRIMARY KEY (`palvelu_id`),
-  INDEX `Palvelu_nimi_index` (`nimi` ASC),
-  INDEX `palv_toimip_id_ind` (`alue_id` ASC),
+  INDEX `Palvelu_nimi_index` (`nimi` ASC) VISIBLE,
+  INDEX `palv_toimip_id_ind` (`alue_id` ASC) VISIBLE,
   CONSTRAINT `palvelu_ibfk_1`
     FOREIGN KEY (`alue_id`)
     REFERENCES `vn`.`alue` (`alue_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
 
 
 -- -----------------------------------------------------
@@ -174,22 +188,21 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `vn`.`varauksen_palvelut` ;
 
 CREATE TABLE IF NOT EXISTS `vn`.`varauksen_palvelut` (
-  `varaus_id` INT(11) UNSIGNED NOT NULL,
-  `palvelu_id` INT(11) UNSIGNED NOT NULL,
-  `lkm` INT(11) NOT NULL,
-  INDEX `vp_varaus_id_index` (`varaus_id` ASC),
-  INDEX `vp_palvelu_id_index` (`palvelu_id` ASC),
+  `varaus_id` INT UNSIGNED NOT NULL,
+  `palvelu_id` INT UNSIGNED NOT NULL,
+  `lkm` INT NOT NULL,
   PRIMARY KEY (`palvelu_id`, `varaus_id`),
-  CONSTRAINT `fk_varaus`
-    FOREIGN KEY (`varaus_id`)
-    REFERENCES `vn`.`varaus` (`varaus_id`)
-    ON DELETE RESTRICT,
+  INDEX `vp_varaus_id_index` (`varaus_id` ASC) VISIBLE,
+  INDEX `vp_palvelu_id_index` (`palvelu_id` ASC) VISIBLE,
   CONSTRAINT `fk_palvelu`
     FOREIGN KEY (`palvelu_id`)
-    REFERENCES `vn`.`palvelu` (`palvelu_id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT)
-ENGINE = InnoDB;
+    REFERENCES `vn`.`palvelu` (`palvelu_id`),
+  CONSTRAINT `fk_varaus`
+    FOREIGN KEY (`varaus_id`)
+    REFERENCES `vn`.`varaus` (`varaus_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_bin;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
